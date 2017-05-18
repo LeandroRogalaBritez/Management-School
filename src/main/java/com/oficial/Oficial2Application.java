2f08e99cd2afcc4a311bd583d81155e92c0e2e52
LeandroRogalaBritez <leandrorogala17@hotmail.com>
2017-05-18 15:11:41 -0300

    Commit Inicial

---
+++ src/main/java/com/oficial/Oficial2Application.java
@@ -0,0 +1,12 @@
+package com.oficial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oficial2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oficial2Application.class, args);
+	}
+}
