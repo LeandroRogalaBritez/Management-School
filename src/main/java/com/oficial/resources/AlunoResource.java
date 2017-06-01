5c3a171be26aa1daa17352a09b4564f5a5a0aeb8
LeandroRogalaBritez <leandrorogala17@hotmail.com>
2017-05-31 22:19:36 -0300

    Comite para Teste

--- src/main/java/com/oficial/resources/AlunoResource.java
+++ src/main/java/com/oficial/resources/AlunoResource.java
@@ -17,34 +17,34 @@ import com.oficial.domain.repository.AlunoRepository;
 public class AlunoResource {
 
 	private AlunoRepository alunoRepository;
-	
+
 	public AlunoResource(AlunoRepository alunoRepository) {
 		this.alunoRepository = alunoRepository;
 	}
-	
+
 	@GetMapping
 	public Iterable<Aluno> getAllAlunos() {
 		return alunoRepository.findAll();
 	}
-	
-	@GetMapping(value="/{id}")
+
+	@GetMapping(value = "/{id}")
 	public Aluno getByName(@PathVariable Long id) {
 		return alunoRepository.findOne(id);
 	}
-	
+
 	@PostMapping
 	public Aluno saveAluno(@RequestBody Aluno aluno) {
 		return alunoRepository.save(aluno);
 	}
-	
-	@PutMapping(value="/{id}")
+
+	@PutMapping(value = "/{id}")
 	public Aluno updateAluno(@PathVariable Long id, @RequestBody Aluno aluno) {
 		return alunoRepository.save(aluno);
 	}
-	
-	@DeleteMapping(value="/{id}")
+
+	@DeleteMapping(value = "/{id}")
 	public void removeAluno(@PathVariable Long id) {
 		alunoRepository.delete(id);
 	}
-	
+
 }
