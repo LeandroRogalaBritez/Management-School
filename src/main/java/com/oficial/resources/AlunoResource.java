6fa6cb60f5a585ae9fc8452e65f6fdd77a51bbda
LeandroRogalaBritez <leandrorogala17@hotmail.com>
2017-05-31 22:27:39 -0300

    Comite teste 5

--- src/main/java/com/oficial/resources/AlunoResource.java
+++ src/main/java/com/oficial/resources/AlunoResource.java
@@ -16,7 +16,7 @@ import com.oficial.domain.repository.AlunoRepository;
 @RequestMapping("/alunos")
 public class AlunoResource {
 
-	private AlunoRepository alunoRepository; //Teste
+	private AlunoRepository alunoRepository;
 
 	public AlunoResource(AlunoRepository alunoRepository) {
 		this.alunoRepository = alunoRepository;
